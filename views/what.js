f717ae67d226165179be5b804046772b01eea622
huston <h@graphalchemist.com>
2014-04-05 17:32:52 -0700

    initial app scaffolding in angular using yeoman

--- views/what.js
+++
@@ -1,7 +0,0 @@
-var HackOre = require('sara')
-
-module.exports = new HackOre.View('What', {
-  render: function () {
-    document.querySelector('main').innerHTML = '<h3></h3>'
-  }
-})
--- views/when.js
+++
@@ -1,7 +0,0 @@
-var HackOre = require('sara')
-
-module.exports = new HackOre.View('When', {
-  render: function () {
-    document.querySelector('main').innerHTML = '<h3></h3>'
-  }
-})
