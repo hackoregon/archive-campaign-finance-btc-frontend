ca22992bdf7c90440e2f601835bc85017093bcef
ninjascience <ninjascience@gmail.com>
2014-08-25 08:50:04 -0700

    working put unpolished bubble chart

--- app/scripts/controllers/campaign.js
+++ app/scripts/controllers/campaign.js
@@ -5,13 +5,20 @@
 
     $scope.campaignId = $routeParams.campaignId;
 
+    var startDate = new Date(2010, 1, 1).getTime();
+    var endDate = new Date(2014, 9, 1).getTime();
+
     $scope.viewModel = {
       section: 'who',
       campaign: [],
       donations: {},
       donationsColorMap: {},
-      startDate: new Date(2010, 10, 1),
-      endDate: new Date(2014, 10, 1)
+      minDate: startDate,
+      maxDate: endDate,
+      startDate: startDate,
+      startFormatted: '',
+      endDate: endDate,
+      endFormatted: ''
     };
 
     $scope.viewModel.donationsColorMap[CampaignService.CONTRIBUTION.PAC] = '#fbb4ae';
@@ -21,27 +28,38 @@
     $scope.viewModel.donationsColorMap[CampaignService.CONTRIBUTION.PARTY] = '#fed9a6';
     $scope.viewModel.donationsColorMap[CampaignService.CONTRIBUTION.NA] = '#ffffcc';
 
-    $http.get('http://54.84.12.11/hackoregon/http/top_committee_data/5/').then(function(result){
-      console.log('GOT IT');
-    });
-
     CampaignService.getCampaign($routeParams.campaignId).then(function(result) {
       $scope.viewModel.campaign = result;
     });
 
-
-
-    CampaignService.getCampaignFinances($scope.viewModel.startDate, $scope.viewModel.endDate).then(function(finances) {
-      var nodes = _(finances.contributions).chain()
-        .map(function(contribution, key){
-          return {category: key, value: contribution.amount};
-        })
-        .filter(function(node){
-          return node.category !== CampaignService.CONTRIBUTION.NA;
-        })
-        .value();
-      $scope.viewModel.donations = {children: nodes};
+    $scope.$watch('viewModel.startDate', function(){
+      $scope.viewModel.startFormatted = new Date(parseInt($scope.viewModel.startDate)).toString()
+      render();
     });
+
+    $scope.$watch('viewModel.endDate', function(){
+      $scope.viewModel.endFormatted = new Date(parseInt($scope.viewModel.endDate)).toString();
+      render();
+    });
+
+    var render = _(function() {
+      var startDate = new Date(parseInt($scope.viewModel.startDate));
+      var endDate = new Date(parseInt($scope.viewModel.endDate));
+      CampaignService.getCampaignFinances(startDate, endDate).then(function(finances) {
+        var nodes = _(finances.contributions).chain()
+          .map(function(contribution, key){
+            return {category: key, value: contribution.amount};
+          })
+          .filter(function(node){
+            return node.category !== CampaignService.CONTRIBUTION.NA;
+          })
+          .value();
+        $scope.viewModel.donations = {children: nodes};
+      });
+    }).throttle(500);
+
+
+
 
   });
 
