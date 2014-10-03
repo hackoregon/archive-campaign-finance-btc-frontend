f7da1c8ea18f10a1a86bdfb6772d2f10756f9597
Paul Fredrickson <p_fredrickson@hotmail.com>
2014-10-02 22:33:02 -0700

    display the percent of money spent
    now that it's supported by the back end

--- app/scripts/directives/simpleRadial.js
+++ app/scripts/directives/simpleRadial.js
@@ -9,40 +9,61 @@
         title: '@',
         icon: '@'
       },
-      controller: function ($scope) {
+      controller: function ($scope, $attrs) {
         $scope.viewModel = {
           formattedPercent: ''
         };
+
+        $scope.radius = ($attrs.size ? parseInt($attrs.size) : 50);
+
+        $scope.positionIcon = function() {
+          var radius = $scope.radius;
+          var half = radius / 2;
+          return {
+            width: radius + 'px',
+            height: radius + 'px',
+            'margin-top': (0-radius) + 'px',
+            position: 'relative',
+            top: (radius + half) + 'px',
+            left: (radius - half) + 'px'
+          }
+        }
+        $scope.colors = [ '#64BCBB', '#D9D9D9' ];
+        if ($attrs.color) {
+          $scope.colors[0] = $attrs.color;
+        }
       },
       link: function(scope, element) {
 
         element.addClass("radial");
 
-        var radius = 50;
+        var radius = scope.radius;
         var svg = d3.select(element.find(".radial")[0]).append("svg")
           .attr("width", "100")
           .attr("height", "100")
         .append("g")
           .attr("transform", "translate("+radius+","+radius+")");
 
-        var colors = [ '#64BCBB', '#D9D9D9' ]
-
         var pie = d3.layout.pie().sort(null);
 
+        var thickness = radius / 5;
         var arc = d3.svg.arc()
-            .innerRadius(radius - 15)
-            .outerRadius(radius -  5);
+            .innerRadius(radius - 5 - thickness)
+            .outerRadius(radius - 5);
 
         var render = function() {
           if (!scope.percent) {
             return;
           }
 
+          // for the visualization, clip to 100%
+          var showPercent = d3.min([scope.percent, 1.0]);
+
           svg.empty();
           svg.selectAll("path")
-              .data( pie([scope.percent, 1.0-scope.percent]) )
+              .data( pie([showPercent, 1.0-showPercent]) )
             .enter().append("path")
-              .attr("fill", function(d, i) { return colors[ (i % colors.length) ]} )
+              .attr("fill", function(d, i) { return scope.colors[ (i % scope.colors.length) ]} )
               .transition().duration(1000).ease('cubic')
               .attrTween("d", function(b) {
                 var i = d3.interpolate({startAngle: 0, endAngle: 2*Math.PI}, b);
