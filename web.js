cda352171d8fcafe662d35054b926365d1072fe4
ninjascience <ninjascience@gmail.com>
2014-10-07 11:58:02 -0700

    trying app

--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var express = require('express');
 var app = express();
  
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/app"));
 app.listen(process.env.PORT || 5000);
