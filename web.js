87c963e1cfee4f486fd789013e1643783456ab6c
ninjascience <ninjascience@gmail.com>
2014-10-07 12:11:38 -0700

    back to dist

--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var express = require('express');
 var app = express();
  
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/app"));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
