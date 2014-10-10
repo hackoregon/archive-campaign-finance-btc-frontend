7c5c1e9e1a41e9bc06141073d815fe447881b8bd
Paul Fredrickson <p_fredrickson@hotmail.com>
2014-10-10 13:41:38 -0700

    update the URL used for querying state-by-state contributions
    code was using an out-of-date endpoint.

--- app/scripts/services/serverConfig.js
+++ app/scripts/services/serverConfig.js
@@ -7,7 +7,7 @@
       var campaignSearchUrl = _.template(BASE_URL + 'candidate_search/<%= searchTerm %>/');
       var transactionsUrl = _.template(BASE_URL + 'current_transactions/<%= campaignId %>/');
       var campaignDetailUrl = _.template(BASE_URL + 'committee_data_by_id/<%= campaignId %>/');
-      var moneyByStateUrl = _.template(BASE_URL + 'candidate_in_by_state/<%= campaignId %>/');
+      var moneyByStateUrl = _.template(BASE_URL + 'candidate_in_by_state_by_id/<%= campaignId %>/');
       var campaignCompetitors = _.template(BASE_URL + 'competitors_from_name/<%= campaignId %>/');
       return {
         campaignSearch: function(searchTerm) {
