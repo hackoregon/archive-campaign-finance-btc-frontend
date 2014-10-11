c555ed9e14a6bec6dab9338ee262cd99ecb24eef
Paul Fredrickson <p_fredrickson@hotmail.com>
2014-10-10 18:40:25 -0700

    finish a cleaned-up/working spending visualization
    showing categories and relative amouts as both a bar chart or a pie chart.
    The pie chart may need work to tease apart the labels when several very
    small slices end up next to each other. For both charts, I went with a
    tooltip that shows up onmouseover because we don't have images for all the
    possible categories.

--- app/scripts/directives/spending.js
+++ app/scripts/directives/spending.js
@@ -10,8 +10,8 @@
       controller: function ($scope, $attrs) {
         $scope.chartType = 'bar';
         $scope.h = $attrs.height || 300; // hmmm, in px :/
-        $scope.w = 100; // in percent
         $scope.dataSet = [];
+        $scope.showTooltip = false;
       },
       link: function(scope, element, attrs) {
 
@@ -19,14 +19,23 @@
           return Math.round(100.0 * val) + '%';
         }
 
-        var h = scope.h;
-        var w = scope.w;
         var padding = { top: 35, left: 10, bottom: 85, right: 10 };
-        var pieColors = d3.scale.category20c();
         var interpColor = d3.interpolateRgb('#D8E4D1', '#49A14C');
+        var themeColors = [
+          '#64BCBB', '#A3D3D2', '#DEEEED', '#E8F5F5',
+          '#49A14C', '#72AE69', '#B5D0AB', '#D8E4D1',
+          '#C89C60', '#DBBF96', '#E6D1B5', '#F0E5D6',
+          '#C3645E', '#CC7E75', '#E1B7AE', '#EDD7D1',
+          '#67576A', '#98869A', '#B4A7B5', '#D4CCD4',
+          '#666666', '#828282', '#BFBFBF', '#D9D9D9'
+        ]
+        function pieColor(i) {
+          return themeColors[ i % themeColors.length ];
+        }
 
         var svg = d3.select(element[0]).select('.d3spend')
-          .append('svg');
+          .append('svg')
+          .attr('width', '100%');
 
         function clearSvg() {
           svg.selectAll('g').remove();
@@ -46,22 +55,45 @@
 
           var yScale = d3.scale.linear()
             .domain([0, maxVal])
-            .range([padding.top, h - padding.bottom - padding.top]);
+            .range([0, scope.h - padding.bottom - padding.top]);
+
+          function fillTooltip(d, element) {
+            var xPosition = parseFloat(d3.select(element).attr('x')) + xScale.rangeBand() / 2;
+            var yPosition = parseFloat(d3.select(element).attr('y')) + 14;
+
+            d3.select('#d3tooltip')
+              .style('left', xPosition + '%')
+              .style('top', yPosition + 'px')
+              .select('#value')
+                .text(d.name);
+          }
 
           clearSvg();
           svg.attr('height', scope.h)
-            .attr('width', scope.w + '%');
-          svg.selectAll('rect')
+            .selectAll('rect')
             .data(dataSet)
             .enter()
               .append('rect')
               .attr('x', function(d,i) { return xScale(i)+'%' })
-              .attr('y', function(d) { return h - yScale(d.amount) - padding.bottom })
+              .attr('y', function(d) { return scope.h - yScale(d.amount) - padding.bottom })
               .attr("width", colWidth + '%')
               .attr('height', function(d) { return yScale(d.amount) })
               .attr('fill', function(d, i) { return interpColor( d.amount / maxVal )})
-              .append('title')
-                .text(function(d) { return d.name });
+              .on('mouseover', function(d) {
+                d3.select(this).attr('fill', '#10716F')
+                fillTooltip(d, this);
+                scope.$apply( function() {
+                  scope.showTooltip = true;
+                })
+              })
+              .on('mouseout', function(d) {
+                d3.select(this)
+                  .attr('fill', function(d, i) { return interpColor( d.amount / maxVal )})
+
+                scope.$apply( function() {
+                  scope.showTooltip = false;
+                })
+              });
 
           svg.selectAll('text')
             .data(dataSet)
@@ -69,20 +101,29 @@
               .append('text')
               .text(function(d) { return formatPercent(d.amount / total) })
               .attr('x', function(d,i) { return xScale(i) + (colWidth/2)+'%' })
-              .attr('y', function(d) { return h - yScale(d.amount) - padding.bottom - 10});
+              .attr('y', function(d) { return scope.h - yScale(d.amount) - padding.bottom - 10})
+              .on('mouseover', function(d) {
+                fillTooltip(d, this);
+                scope.$apply( function() {
+                  scope.showTooltip = true;
+                })
+              })
+              .on('mouseout', function(d) {
+                scope.$apply( function() {
+                  scope.showTooltip = false;
+                })
+              });
         }
         function renderPie() {
           var dataSet = scope.dataSet;
           var total = d3.sum(dataSet, function(d) { return d.amount} );
 
           clearSvg();
-          svg.attr('height', scope.h)
-            .attr('width', scope.w + '%');
+          svg.attr('height', scope.h + padding.top + padding.bottom);
 
           var r = scope.h / 2;
-          var g = svg.attr('height', 2*r)
-            .append('g')
-            .attr('transform', 'translate('+r+','+r+')')
+          var g = svg.append('g')
+            .attr('transform', 'translate('+(r+padding.left+50)+','+(r+padding.top)+')')
             .data([dataSet])
 
           var pie = d3.layout.pie().sort(null).value( function(d) { return d.amount });
@@ -93,14 +134,47 @@
               .append("g")
                 .attr("class", "slice");
 
+          function fillTooltip(d, element) {
+            var pos = arc.centroid(d);
+            d3.select('#d3tooltip')
+              .style('left', pos[0] + r + 'px')
+              .style('top', (pos[1] + r - 14) + 'px')
+              .select('#value')
+                .text(d.data.name);
+          }
+
+          arcs.each(function(d) {
+            var a = d.startAngle + (d.endAngle - d.startAngle) / 2 - Math.PI / 2;
+            if ((d.value / total) < .05) {
+              d.cx = Math.cos(a) * (r + 20);
+              d.cy = Math.sin(a) * (r + 20);
+            }
+          })
           arcs.append("path")
-            .attr("fill", function(d, i){ return pieColors(i); })
-            .attr("d", function (d) {
-              return arc(d);
+            .attr("fill", function(d, i){ return pieColor(i); })
+            .attr("d", function (d) { return arc(d); })
+            .on('mouseover', function(d) {
+              d3.select(this).attr('fill', '#10716F')
+              fillTooltip(d, this);
+              scope.$apply( function() {
+                scope.showTooltip = true;
+              })
+            })
+            .on('mouseout', function(d, i) {
+              var color = pieColor(i);
+              d3.select(this)
+                .attr('fill', color);
+
+              scope.$apply( function() {
+                scope.showTooltip = false;
+              })
             });
 
           arcs.append("text")
             .attr("transform", function(d) {
+              if (d.cx && d.cy) {
+                return "translate(" + d.cx + "," + d.cy + ")";
+              }
               d.innerRadius = 0;
               d.outerRadius = r;
               return "translate(" + arc.centroid(d) + ")";
